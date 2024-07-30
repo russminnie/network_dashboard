f7ab7ddf69c4399a77c2eabbe63a5b4199467531
Benjamin Lindeen <blindeen@multitech.com>
2024-07-30 16:05:46 -0500

    help for menus

---
+++ static/js/connect_form.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.help-button').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const modalId = event.target.getAttribute('data-modal');
+            openHelpModal(modalId);
+        });
+    });
+
+    window.onclick = function (event) {
+        const modals = document.querySelectorAll('.modal');
+        modals.forEach(modal => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
+});
+
+function openHelpModal(modalId) {
+    const helpModal = document.getElementById(modalId);
+    if (helpModal) {
+        helpModal.style.display = 'block';
+    }
+}
+
+function closeHelpModal(modalId) {
+    const helpModal = document.getElementById(modalId);
+    if (helpModal) {
+        helpModal.style.display = 'none';
+    }
+}
+
+window.onclick = function (event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
